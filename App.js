ecc9da3c71e11b9f5a410fd678e9e0d168d360c4
Michael H. Potter <michael@potterpcs.net>
2019-01-19 23:14:05 -0500

    Added NativeBase to deps and made sure it works

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Content, Container, Header, Footer } from 'native-base';
 
 export default class App extends React.Component {
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+    });
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Container style={styles.container}>
+        <Header></Header>
+        <Content style={{alignSelf: 'center'}}>
+            <Text>This is the beginning...</Text>
+        </Content>
+        <Footer></Footer>
+      </Container>
     );
   }
 }
@@ -15,7 +27,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'stretch',
+    justifyContent: 'flex-start',
   },
 });
